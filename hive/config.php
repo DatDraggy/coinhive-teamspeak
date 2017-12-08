30ecd4078be8c7e425ef5437283545a04d1eceab
Kieran <bmxstreet@gmx.net>
2017-12-08 19:40:46 +0100

    Added debug option
    To test if server groups can be given and connection can be established

--- hive/config.php
+++ hive/config.php
@@ -19,6 +19,7 @@ $config["ip"] = "1.1.1.1"; //TS Server IP/Domain
 $config["ports"] = array("9987", "9988", "9989"); //TS Server Port
 $config["qPort"] = "10011"; // TS Query Port, Default 10011
 $config["nickname"] = rawurlencode("Bot_" . srandom(5)); // Random name to prevent double nickname error
+$config["debugPass"] = ""; //Passwort for debug. Leave empty for security if not needed
 
 $config["dbserver"] = "localhost"; //MySQL Server
 $config["dbuser"] = "user"; //MySQL User
---
+++ hive/testconn.php
@@ -0,0 +1,44 @@
+<?php
+require_once('./TeamSpeak3/TeamSpeak3.php');
+require_once('./config.php');
+echo '<!DOCTYPE html><html><head><script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script><meta charset="utf-8"></head><body>';
+try {
+    $comboboxValues = '<option value="">Users</option>';
+    foreach($config["ports"] as $port) {
+        $ts3 = TeamSpeak3::factory("serverquery://" . $config["username"] . ":" . $config["password"] . "@" . $config["ip"] . ":" . $config["qPort"] . "/?server_port=" . $port . "&nickname=" . $config["nickname"] . "");
+        $arr_ClientList = $ts3->clientList(array("client_type" => 0));
+        foreach ($arr_ClientList as $client) {
+            $comboboxValues = $comboboxValues . '<option value="' . $client['client_unique_identifier'] . '|||' . $client['client_nickname'] . '">' . $client['client_nickname'] . ' (' . $client['client_unique_identifier'] . ')</option>';
+        }
+        $ts3 = null;
+    }
+    echo '<select id="names">';
+    echo $comboboxValues;
+    echo "</select><br>";
+} catch (TeamSpeak3_Exception $error) {
+    echo "Error " . $error->getCode() . ": " . $error->getMessage() . "<br><br>" . $error . "<br><br>";
+}
+?>
+<br><input type="password" id="password" placeholder="Debug Password"><br>
+<button id="add_group" style="margin-bottom:50px">Add to group</button><br>
+<span id="error"></span>
+<script>
+    $("#add_group").click(function() {
+        /* Ajax call script add UID to group for test */
+        var user = $('#names').find(":selected").val();
+        $.ajax({
+            type: "POST",
+            url: "testconnB.php",
+            data: "user=" + user + "&password=" + $('#password').val(),
+            success: function (text) {
+                $('#error').html(text);
+            },
+            error: function () {
+                $('#error').html("Couldn't perform ajax call");
+            }
+        });
+    });
+</script>
+
+</body>
+</html>
